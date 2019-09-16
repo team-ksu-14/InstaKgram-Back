7f12e50830bd78b0be7e9117df5601a83b49b5a3
woghd9072 <woghd9072@gmail.com>
2019-09-16 11:01:15 +0900

    팔로잉 팔로워 조회 구현

--- src/main/java/me/instakgram/boot/follow/FollowingService.java
+++ src/main/java/me/instakgram/boot/follow/FollowingService.java
@@ -1,10 +1,46 @@
 package me.instakgram.boot.follow;
 
 import lombok.RequiredArgsConstructor;
+import me.instakgram.boot.member.Member;
+import me.instakgram.boot.member.MemberRepository;
+import org.springframework.hateoas.Resources;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @Service
 @RequiredArgsConstructor
 public class FollowingService {
+
+    private final MemberRepository memberRepository;
+
+    private final EntityManager em;
+
+    public Resources getFollowingList(Long idx) {
+        String sql = "Select f From Member m Join m.followers f Where m.id = :idx";
+        TypedQuery<Member> query = em.createQuery(sql, Member.class);
+        query.setParameter("idx", idx);
+
+        List<Member> followings = query.getResultList();
+        Resources<Member> resources = new Resources<>(followings);
+        resources.add(linkTo(methodOn(FollowingController.class).getFollwings(idx)).withSelfRel());
+        return resources;
+    }
+
+    public Resources getFollowerList(Long idx) {
+        String sql = "Select m From Member m Join m.followers f Where f.id = :idx";
+        TypedQuery<Member> query = em.createQuery(sql, Member.class);
+        query.setParameter("idx", idx);
+
+        List<Member> followers = query.getResultList();
+        Resources<Member> resources = new Resources<>(followers);
+        resources.add(linkTo(methodOn(FollowingController.class).getFollowers(idx)).withSelfRel());
+        return resources;
+    }
 
 }
